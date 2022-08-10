6bb5b4bac0feb6d797734761b4825e56c89a049f
shraddha46-web <indulkar.shraddha@gmail.com>
2022-08-10 20:01:40 +0530

    reverted changes

--- src/component/NewsWrapper.js
+++ src/component/NewsWrapper.js
@@ -35,11 +35,11 @@ export default function NewsWrapper(props) {
 
   function getNews(pageNo) {
     // //By default all top headlines news without any category
-    let newsUrl = `http://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=${props.api_key}&page=${pageNo}&pageSize=${props.pageSize}`;
+    let newsUrl = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=${props.api_key}&page=${pageNo}&pageSize=${props.pageSize}`;
 
     //All top news with selected category
     if (props.category !== "top headlines") {
-      newsUrl = `http://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.api_key}&page=${pageNo}&pageSize=${props.pageSize}`;
+      newsUrl = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.api_key}&page=${pageNo}&pageSize=${props.pageSize}`;
     }
 
     setIsloader(true);
@@ -57,11 +57,11 @@ export default function NewsWrapper(props) {
   const fetchMoreData = async () => {
     let pageNo = pageNumber + 1;
     // //By default all top headlines news without any category
-    let newsUrl = `http://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=${props.api_key}&page=${pageNo}&pageSize=${props.pageSize}`;
+    let newsUrl = `https://newsapi.org/v2/top-headlines?country=${props.country}&apiKey=${props.api_key}&page=${pageNo}&pageSize=${props.pageSize}`;
 
     //All top news with selected category
     if (props.category !== "top headlines") {
-      newsUrl = `http://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.api_key}&page=${pageNo}&pageSize=${props.pageSize}`;
+      newsUrl = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.api_key}&page=${pageNo}&pageSize=${props.pageSize}`;
     }
 
     await fetch(newsUrl)
